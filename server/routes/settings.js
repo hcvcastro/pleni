38350e20c20eab247e0f61ee81b3c2a800b40536
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-07-31 00:17:03 -0400

    test of couchdb connector

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -11,13 +11,47 @@ exports.testdb=function(req,res,next){
     // validation
     var validator=require('validator');
     if((!validator.isIP(host)&&!validator.isURL(host))||!validator.isInt(port)){
-        res.json({test:false});
+        res.json({
+            test:false,
+            message:'Validation error'
+        });
         return;
     }
 
     // testing couchdb
-    // TODO
-
-    res.json({test:true});
-}
+    require('http').get({
+        host:host.replace(/.*?:\/\//g, ""),
+        port:port,
+        path:'/'
+    },function(res2){
+        res2.setEncoding('utf8')
+        res2.on('data',function(body){
+            if(validator.isJSON(body)){
+                var obj=JSON.parse(body);
+                if('couchdb' in obj){
+                    res.json({
+                        test:true,
+                        message:'Success connection',
+                        version:obj.version
+                    });
+                }else{
+                    res.json({
+                        test:false,
+                        message:'JSON error'
+                    });
+                }
+            }else{
+                res.json({
+                    test:false,
+                    message:'Connection error'
+                });
+            }
+        });
+    }).on('error',function(error){
+        res.json({
+            test:false,
+            message:'Network error'
+        });
+    });
+};
 
