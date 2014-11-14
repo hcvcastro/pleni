8e20bcc8810a309810fa32551ab8971a9d74d4e3
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-11-14 07:42:08 -0400

    creation of interface for resources

--- master/public/js/app.js
+++ master/public/js/app.js
@@ -24,5 +24,22 @@ var pleniApp=angular.module('PleniApp',['ngRoute','ngResource','ngStorage'])
     function($scope,$sessionStorage){
     $('header nav ul li').removeClass('active');
     $('header nav ul li:nth-child(1)').addClass('active');
+
+    $scope.show_dbservers=function(){
+        $('section.dbservers').addClass('active')
+            .siblings().removeClass('active');
+    }
+    $scope.show_repositories=function(){
+        $('section.repositories').addClass('active')
+            .siblings().removeClass('active');
+    }
+    $scope.show_planners=function(){
+        $('section.planners').addClass('active')
+            .siblings().removeClass('active');
+    }
+    $scope.show_iopipes=function(){
+        $('section.iopipes').addClass('active')
+            .siblings().removeClass('active');
+    }
 }])
 
