8f983abad056f1c3c0afedd654a0307d153f6e83
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-11-25 12:49:05 -0400

    working on validation method

--- master/controllers/resources.js
+++ master/controllers/resources.js
@@ -3,45 +3,7 @@
 var validate=require('../../planners/utils/validators')
   , _success=require('../../planners/utils/json-response').success
   , _error=require('../../planners/utils/json-response').error
-  , schemas={
-        dbserver:{
-            'type':'object'
-          , 'properties':{
-                'id':{
-                    'type':'string'
-                }
-              , 'db':{
-                    'type':'object'
-                  , 'properties':{
-                        'host':{
-                            'type':'string'
-                        }
-                      , 'port':{
-                            'type':'string'
-                        }
-                      , 'user':{
-                            'type':'string'
-                        }
-                      , 'pass':{
-                            'type':'string'
-                        }
-                      , 'prefix':{
-                            'type':'string'
-                        }
-                    }
-                  , 'required':['host','port','user','pass','prefix']
-                }
-            }
-          , 'required':['id','db']
-        }
-      , dbservers:{
-            '$schema':'http://json-schema.org/draft-04/schema#'
-          , 'type':'array'
-          , 'items':schemas.dbserver
-          , 'minItems':1
-          , 'uniqueItems':true
-        }
-    }
+  , schema=require('../utils/schema')
 
 module.exports=function(app){
     app.get('/resources/view',function(request,response){
@@ -68,30 +30,21 @@ module.exports=function(app){
     });
 
     app.put('/resources/dbservers',function(request,response){
-        var jayschema=require('jayschema')
-          , js=new jayschema()
-
-        if(js.validate(request.body,schemas.dbservers).length==0){
+        if(schema.js.validate(request.body,schema.dbservers).length==0){
             var resources=app.get('resources')
               , list=new Array()
 
             for(var i in request.body){
-                if(validate.validSlug(request.body[i].id)
-                    &validate.validHost(request.body[i].db.host)
-                    &validate.validPort(request.body[i].db.port)
-                    &validate.validSlug(request.body[i].db.user)
-                    &validate.validSlug(request.body[i].db.prefix)){
-                    list.push({
-                        id:validate.toString(request.body[i].id)
-                      , db:{
-                            host:validate.toValidHost(request.body[i].db.host)
-                          , port:validate.toInt(request.body[i].db.port)
-                          , user:validate.toString(request.body[i].db.user)
-                          , pass:validate.toString(request.body[i].db.pass)
-                          , prefix:validate.toString(request.body[i].db.prefix)
-                        }
-                    });
-                }
+                list.push({
+                    id:validate.toString(request.body[i].id)
+                  , db:{
+                        host:validate.toValidHost(request.body[i].db.host)
+                      , port:validate.toInt(request.body[i].db.port)+''
+                      , user:validate.toString(request.body[i].db.user)
+                      , pass:validate.toString(request.body[i].db.pass)
+                      , prefix:validate.toString(request.body[i].db.prefix)
+                    }
+                });
             }
             
             resources.dbservers=list
@@ -103,31 +56,34 @@ module.exports=function(app){
     });
 
     app.post('/resources/dbservers',function(request,response){
-        var jayschema=require('jayschema')
-          , js=new jayschema()
-
-        if(js.validate(request.body,schemas.dbserver).length==0){
+        if(schema.js.validate(request.body,schema.dbserver).length==0){
             var resources=app.get('resources')
-              , id=validate.toString(request.body.id)
-              , repositories=app.get('repositories')
-              , repository=get_repository(id,repositories)
-              , new_repository={
-                    id:     id
-                  , host:   validate.toValidHost(request.body.host)
-                  , port:   validate.toInt(request.body.port)
-                  , dbuser: validate.toString(request.body.dbuser)
-                  , dbpass: validate.toString(request.body.dbpass)
-                  , prefix: validate.toString(request.body.prefix)
-                }
+              , dbservers=resources.dbservers
+              , dbserver=get_repository(request.body.id,dbservers)
 
-            if(!repository){
-                repositories.push(new_repository);
-                app.set('repositories',repositories);
+            if(!dbserver){
+                var new_dbserver={
+                    id:request.body.id
+                  , db:{
+                        host:validate.toValidHost(request.body.db.host)
+                      , port:validate.toInt(request.body.db.port)+''
+                      , user:validate.toString(request.body.db.user)
+                      , pass:validate.toString(request.body.db.pass)
+                      , prefix:validate.toString(request.body.db.prefix)
+                    }
+                };
+
+                dbservers.push(new_dbserver);
+                resources.dbservers=dbservers;
+                app.set('resources',resources);
+
                 response.status(201).json({
-                    id:     new_repository.id
-                  , host:   new_repository.host
-                  , port:   new_repository.port
-                  , prefix: new_repository.prefix
+                    id:new_dbserver.id
+                  , db:{
+                        host:new_dbserver.db.host
+                      , port:new_dbserver.db.port
+                      , prefix:new_dbserver.db.prefix
+                    }
                 });
             }else{
                 response.status(403).json(_error.notoverride);
@@ -136,5 +92,15 @@ module.exports=function(app){
             response.status(403).json(_error.validation);
         }
     });
+
+    var get_repository=function(needle,haystack){
+        for(var i in haystack){
+            if(haystack[i].id==needle){
+                return [i,haystack[i]];
+            }
+        }
+        return null;
+    };
+
 };
 
---
+++ master/utils/schema.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var validator=require('validator')
+  , jayschema=require('jayschema')
+  , js=new jayschema()
+  , notEmpty=function(element){
+    if(typeof element==='undefined'){
+        return false;
+    }
+    if(element===null){
+        return false;
+    }
+    if(element===''){
+        return false;
+    }
+    return true;
+};
+
+js.addFormat('id',function(value){
+    if(notEmpty(value)
+        && /^[a-z][a-z0-9_]*\/?[a-z][a-z0-9_]*$/i.test(value)){
+        return null;
+    }
+    return 'id is not a valid string';
+});
+js.addFormat('host',function(value){
+    if(notEmpty(value)
+        && (validator.isIP(value)||validator.isURL(value))){
+        return null;
+    }
+    return 'host is not a valid';
+});
+js.addFormat('port',function(value){
+    var _port=validator.toInt(value);
+    if(notEmpty(value)
+        && validator.isInt(value)
+        && (0 < _port && _port < 65536)){
+        return null;
+    }
+    return 'port is not a valid';
+});
+js.addFormat('slug',function(value){
+    if(notEmpty(value)
+        && /^[a-z][a-z0-9_]*$/i.test(value)){
+        return null;
+    }
+    return 'there is not a valid slug';
+});
+exports.js=js;
+
+var dbserver={
+    'type':'object'
+  , 'properties':{
+        'id':{
+            'type':'string'
+          , 'format':'id'
+        }
+      , 'db':{
+            'type':'object'
+          , 'properties':{
+                'host':{
+                    'type':'string'
+                  , 'format':'host'
+                }
+              , 'port':{
+                    'type':'string'
+                  , 'format':'port'
+                }
+              , 'user':{
+                    'type':'string'
+                  , 'format':'slug'
+                }
+              , 'pass':{
+                    'type':'string'
+                }
+              , 'prefix':{
+                    'type':'string'
+                  , 'format':'slug'
+                }
+            }
+          , 'required':['host','port','user','pass','prefix']
+        }
+    }
+  , 'required':['id','db']
+};
+exports.dbserver=dbserver;
+
+var dbservers={
+    'type':'array'
+  , 'items':dbserver
+  , 'minItems':1
+  , 'uniqueItems':true
+};
+exports.dbservers=dbservers;
+
