b209b1e8487e7c251b08493876a77174a003ff53
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-01-06 22:09:33 -0400

    working on notifier control for master app

--- master/public/js/controllers/notifier.js
+++ master/public/js/controllers/notifier.js
@@ -1,13 +1,21 @@
 'use strict';
 
 pleni.controller('SocketController',
-    ['$scope','$sessionStorage','Socket',
-    function($scope,$sessionStorage,Socket){
+    ['$scope','$sessionStorage','$http','Socket',
+    function($scope,$sessionStorage,$http,Socket){
         $scope.storage=$sessionStorage;
+        $scope.threads=[];
+
+        $scope.init=function(){
+            $http.get('/notifier').success(function(data){
+                $scope.threads=data;
+            });
+        };
 
         Socket.on('notifier',function(msg){
             console.log(msg);
         });
+        $scope.init();
     }]
 );
 
