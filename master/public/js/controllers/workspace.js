20651d98cbfe1db9e0553c3374e65197e0f79e80
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-01-17 23:24:26 -0400

    working on settings for workspace

--- master/public/js/controllers/workspace.js
+++ master/public/js/controllers/workspace.js
@@ -4,8 +4,22 @@ pleni.controller('WorkspaceController',
     ['$scope','$routeParams','$location','$sessionStorage','$http',
     function($scope,$routeParams,$location,$sessionStorage,$http){
         $scope.storage=$sessionStorage;
+
+        var get_element=function(needle,haystack){
+                for(var i in haystack){
+                    if(haystack[i].id==needle){
+                        return [i,haystack[i]];
+                    }
+                }
+                return;
+            }
+          , project=$routeParams.project
+          , repositories=get_element(project,$scope.storage.projects)[1]
+
         $scope.storage.workspace={
-            project:$routeParams.project
+            name:project
+          , repositories:repositories._repositories
+          , settings:true
         };
 
         $scope.workspace={
