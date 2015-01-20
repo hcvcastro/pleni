371cb0abf3034013572024903fce90a577b688b9
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-01-20 14:42:27 -0400

    working on settings for workspace

--- master/public/js/controllers/workspace.js
+++ master/public/js/controllers/workspace.js
@@ -3,32 +3,87 @@
 pleni.controller('WorkspaceController',
     ['$scope','$routeParams','$location','$sessionStorage',
     'Resources','Projects',
-    function($scope,$routeParams,$location,$sessionStorage,Resouces,Projects){
+    function($scope,$routeParams,$location,$sessionStorage,Resources,Projects){
         $scope.storage=$sessionStorage;
 
-        if($routeParams.project){
-            $scope.storage.workspace={
-                name:$routeParams.project
-            };
-        }else{
-            $scope.workspace.close();
+        if(!$routeParams.project){
+            $location.path('/projects');
         }
 
-        Projects.get({project:$scope.storage.workspace.name},
-        function(data){
-            $scope.storage.workspace.repositories=data._repositories;
-        },function(error){});
-
-        if(!$scope.storage.workspace.planners){
-            $scope.storage.workspace.panel='settings';
-        }
+        $scope.storage.workspace={
+            name:$routeParams.project
+          , panel:'settings'
+        };
 
         $scope.workspace={
-            close:function(){
+            init:function(){
+                Projects.get({
+                    project:$scope.storage.workspace.name
+                },function(data){
+                    $scope.storage.workspace.repositories=data._repositories;
+                },function(error){});
+            }
+          , close:function(){
                 delete $scope.storage.workspace;
                 $location.path('/projects');
             }
+          , accept:function(){
+                console.log('accept');
+            }
+          , cancel:function(){
+                console.log('cancel');
+            }
         };
+
+        $scope.planners={
+            load:function(){
+                Resources.planners.load(function(data){
+                    $scope.storage.planners.forEach(function(planner){
+                        planner.check='checking';
+                        Resources.planners.check({
+                            server:planner.id
+                        },function(data){
+                            planner.check='online';
+                            planner.type=data.planner.type;
+
+                            Resources.planners.status({
+                                server:planner.id
+                            },function(data){
+                                planner.status=data.planner.status;
+                            },function(error){
+                                planner.status='unknown';
+                            });
+
+                            Resources.planners.isset({
+                                server:planner.id
+                            },function(data){
+                                if(data.planner.result){
+                                    $scope.planners.get();
+                                }else{
+                                    planner.set.status='unset';
+                                }
+                            },function(error){
+                                planner.set.status='unknown';
+                            });
+                        },function(error){
+                            planner.check='offline';
+                        });
+                    });
+                });
+            }
+          , follow:function(index){
+                console.log('follow instruction');
+            }
+          , check:function(index){
+                console.log('check instruction');
+            }
+          , exclusive:function(index){
+                console.log('exclusive instruction');
+            }
+        };
+
+        $scope.workspace.init();
+        $scope.planners.load();
     }]
 );
 
