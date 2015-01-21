8c7633077bf2f7efd6db70694363bd7001e81f33
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-01-21 12:09:27 -0400

    working on workspace functions

--- master/public/js/controllers/workspace.js
+++ master/public/js/controllers/workspace.js
@@ -49,62 +49,116 @@ pleni.controller('WorkspaceController',
         $scope.planners={
             load:function(){
                 Resources.planners.load(function(data){
-                    $scope.storage.planners.forEach(function(planner){
-                        planner.check='checking';
-                        Resources.planners.check({
-                            server:planner.id
-                        },function(data){
-                            planner.check='online';
-                            planner.type=data.planner.type;
-
-                            Resources.planners.status({
-                                server:planner.id
-                            },function(data){
-                                planner.status=data.planner.status;
-                            },function(error){
-                                planner.status='unknown';
-                            });
-
-                            Resources.planners.isset({
-                                server:planner.id
-                            },function(data){
-                                if(data.planner.result){
-                                    $scope.planners.get();
-                                }else{
-                                    planner.set.status='unset';
-                                }
-                            },function(error){
-                                planner.set.status='unknown';
-                            });
-
-                            Resources.planners.api({
-                                server:planner.id
-                            },function(data){
-                                planner.api=data.planner.tasks;
-                                planner.api.forEach(function(task){
-                                    if($scope.storage.workspace.api[task.name]){
-                                        $scope.storage.workspace
-                                              .api[task.name].push(planner);
-                                    }else{
-                                        $scope.storage.workspace
-                                              .api[task.name]=[planner];
-                                    }
-                                });
-                            },function(error){});
-                        },function(error){
-                            planner.check='offline';
-                        });
-                    });
+                    for(var index in $scope.storage.planners){
+                        $scope.planners.check(index);
+                    }
                 });
             }
           , follow:function(index){
-                console.log('follow instruction');
+                var planner=$scope.storage.planners[index]
+                  , notifier='master'
+
+                if(planner.follow){
+                    Resources.notifiers.remove({
+                        server:notifier
+                      , planner:planner.id
+                    },function(data){
+                        $scope.storage.planners[index].follow=false;
+                    },function(error){});
+                }else{
+                    Resources.notifiers.add({
+                        server:notifier
+                      , planner:planner.id
+                    },function(data){
+                        $scope.storage.planners[index].follow=true;
+                    },function(error){});
+                }
             }
           , check:function(index){
-                console.log('check instruction');
+                var planner=$scope.storage.planners[index];
+
+                planner.check='checking';
+                Resources.planners.check({
+                    server:planner.id
+                },function(data){
+                    planner.check='online';
+                    planner.type=data.planner.type;
+
+                    Resources.planners.status({
+                        server:planner.id
+                    },function(data){
+                        planner.status=data.planner.status;
+                    },function(error){
+                        planner.status='unknown';
+                    });
+
+                    Resources.planners.isset({
+                        server:planner.id
+                    },function(data){
+                        if(data.planner.result){
+                            $scope.planners.get(index);
+                        }else{
+                            planner.set.status='unset';
+                        }
+                    },function(error){
+                        planner.set.status='unknown';
+                    });
+
+                    Resources.planners.api({
+                        server:planner.id
+                    },function(data){
+                        planner.api=data.planner.tasks;
+                        planner.api.forEach(function(task){
+                            if($scope.storage.workspace.api[task.name]){
+                                $scope.storage.workspace
+                                      .api[task.name].push(planner);
+                            }else{
+                                $scope.storage.workspace
+                                      .api[task.name]=[planner];
+                            }
+                        });
+                    },function(error){});
+                },function(error){
+                    planner.check='offline';
+                });
+            }
+          , get:function(index){
+                var planner=$scope.storage.planners[index];
+
+                Resources.planners.get({
+                    server:planner.id
+                },function(data){
+                    planner.set.status='set';
+                    planner.set.name=data.planner.task.name;
+                    planner.set.count=data.planner.task.count;
+                    planner.set.interval=data.planner.task.interval;
+                    if(planner.api){
+                        for(var i=0;i<planner.api.length;i++){
+                            if(planner.set.name==
+                                planner.api[i].name){
+                                planner.set.schema=planner.api[i].schema;
+                                break;
+                            }
+                        }
+                    }
+//                    $scope.planners.editor();
+                },function(error){});
             }
           , exclusive:function(index){
-                console.log('exclusive instruction');
+                var planner=$scope.storage.planners[index];
+
+                Resources.planners.set({
+                    server:planner.id
+                  , task:{
+                        name:'exclusive'
+                      , count:1
+                      , interval:500
+                    }
+                },function(data){
+                    console.log(data);
+                },function(error){
+                    console.log(error);
+                });
             }
         };
 
