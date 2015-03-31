fe4fdfd3fac26f6fa7a782a14124ba961871798c
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-03-31 14:05:21 -0400

    working on report generation

--- scripts/planner_insert_design.sh
+++ scripts/planner_insert_design.sh
@@ -3,5 +3,5 @@
 HOST='http://jacobian:asdf@localhost:5984'
 DBNAME=${1}
 
-curl -X PUT $HOST/$DBNAME/_design/analytics -d @mapreduce.json
+curl -X PUT $HOST/$DBNAME/_design/reports -d @mapreduce.json
 
