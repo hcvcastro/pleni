c2503e631c13c238fd8d0daceb92947854d30578
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-05-03 14:12:23 -0400

    working on static pages generation

--- server/master/home.js
+++ server/master/home.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var fs=require('fs')
+  , join=require('path').join
+  , _success=require('../../core/json-response').success
+  , _error=require('../../core/json-response').error
+
 module.exports=function(app){
     app.get('/',function(request,response){
         if(request.user){
@@ -12,6 +17,20 @@ module.exports=function(app){
 
     app.get('/home',function(request,response){
         response.render('pages/home');
+    });
+
+    app.get('/static/:page',function(request,response){
+        var base=join(__dirname,'..','..','client','views','master','static');
+
+        fs.readdir(base,function(err,files){
+            if(!err&&files.some(function(file){
+                return file.substring(0,file.length-5)===request.params.page;
+            })){
+                response.render('static/'+request.params.page);
+            }else{
+                response.status(404).json(_error.notfound);
+            }
+        });
     });
 };
 
--- tests/master/home.js
+++ tests/master/home.js
@@ -3,17 +3,49 @@
 var request=require('supertest')
   , should=require('should')
   , app=require('../../server/master')
+  , _success=require('../../core/json-response').success
+  , _error=require('../../core/json-response').error
 
 describe('home controller functions',function(){
     it('GET /',function(done){
         request(app)
             .get('/')
+            .expect('Content-Type',/html/)
             .expect(200,done);
     });
+
     it('GET /home',function(done){
         request(app)
             .get('/home')
+            .expect('Content-Type',/html/)
             .expect(200,done);
+    });
+
+    it('GET /static/notfound',function(done){
+        request(app)
+            .get('/static/notfound')
+            .expect('Content-Type',/json/)
+            .expect(404)
+            .end(function(err,res){
+                res.statusCode.should.be.eql(404);
+                res.should.be.json;
+                res.body.should.eql(_error.notfound);
+                done();
+            });
+    });
+
+    ['api']
+    .forEach(function(element){
+        it('GET /static/'+element,function(done){
+            request(app)
+                .get('/static/'+element)
+                .expect('Content-Type',/html/)
+                .expect(200)
+                .end(function(err,res){
+                    res.statusCode.should.be.eql(200);
+                    done();
+                });
+        });
     });
 });
 
