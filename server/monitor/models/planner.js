8ae1e32cc91410cb456a0d52cbeda4803f0f9ba9
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-05-26 16:46:19 -0400

    define the model

---
+++ server/monitor/models/planner.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var mongoose=require('mongoose')
+  , Schema=mongoose.Schema
+
+var planner=new Schema({
+    host:{
+        type:String
+      , required:true
+      , lowercase:true
+    }
+  , port:Number
+  , tid:{
+        type:String
+      , required:false
+    }
+  , status:{
+        type:String
+      , required:false
+      , default:'stopped'
+      , enum:['running','stopped']
+    }
+});
+
+module.exports=mongoose.model('Planner',planner);
+
