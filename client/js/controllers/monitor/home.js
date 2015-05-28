94c119d4d8151f19ba86c5d3703abb6ea1d012b6
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-05-27 23:34:19 -0400

    working on planners viewers

--- client/js/controllers/monitor/home.js
+++ client/js/controllers/monitor/home.js
@@ -1,32 +1,29 @@
 'use strict';
 
-pleni.controller('HomeController',['$rootScope','$scope','Auth',
-    function($rootScope,$scope,Auth){
+pleni.controller('HomeController',
+    ['$scope','$sessionStorage','Auth','Resources',
+    function($scope,$sessionStorage,Auth,Resources){
     utils.set_tab(0,1);
     utils.set_header(true);
 
-    if(Auth.isUser('monitr')){
-        utils.set_active('manager',1);
-    }else{
+    if(!Auth.isUser('monitr')){
         utils.set_active('signin',1);
+
+        $('input[name=email]').focus();
+
+        $scope.email='';
+        $scope.password='';
+
+        $scope.signin=function(){
+            utils.send('Trying login into account ...');
+            Auth.signin($scope.email,$scope.password,
+                $('input[name=_csrf]').val(),
+                function(){
+                    location.reload();
+                });
+        };
+    }else{
+        $scope.storage=$sessionStorage;
     }
-
-    $('input[name=email]').focus();
-
-    if($rootScope.flash){
-        utils.show($rootScope.flash[0],$rootScope.flash[1]);
-        delete $rootScope.flash;
-    }
-
-    $scope.email='';
-    $scope.password='';
-
-    $scope.signin=function(){
-        utils.send('Trying login into account ...');
-        Auth.signin($scope.email,$scope.password,$('input[name=_csrf]').val(),
-            function(){
-                location.reload();
-            });
-    };
 }]);
 
